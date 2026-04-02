c77ecf17f0c136ffeb2c9f91948676e4d8cfd60f
nghia-chien <hacphichien@gmail.com>
2026-04-03 00:07:31 +0700

    deploy frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/hotel_booking/",
   server: {
      proxy: {
       "/api": "http://localhost:3000"
